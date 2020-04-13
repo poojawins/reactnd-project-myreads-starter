73d3fbf474ba7ee0ab33919dc67a42c3c861e13c
Pooja Pasawala <pooja@gladly.com>
2020-04-13 00:02:03 -0700

    Add backgroundImage for Book component.

--- src/Book.js
+++ src/Book.js
@@ -4,7 +4,7 @@ function Book(props) {
   return(
     <div className='book'>
       <div className='book-top'>
-        <div className='book-cover' style={{ width: 128, height: 193 }}></div>
+        <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
         <div className='book-shelf-changer'>
           <select>
             <option value='move' disabled>Move to...</option>
